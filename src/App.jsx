d798fe5f625fd9f0b6f682315b1091287352cf4a
adanda23 <abhiramdanda@gmail.com>
2024-04-20 18:50:05 -0500

    Added video wallpaper and example pictures. Get pictures button fetches picture.

--- src/App.jsx
+++ src/App.jsx
@@ -2,31 +2,43 @@ import { useState } from 'react'
 
 function App() {
 
-  const [buttonText, setButtonText] = useState('Find Heroes');
-  const [heroData, setHeroData] = useState(null);
+  const [buttonText, setButtonText] = useState('Get Pictures');
+  const [heroData, setHeroData] = useState("https://api.nasa.gov/planetary/earth/imagery?lon=-90.33&lat=30.78&date=2018-01-01&dim=0.15&api_key=sFbPQNVBu4cflCZxqlKGxC6ZWiakIWhqiMXNOatX");
+  const apiKey = "sFbPQNVBu4cflCZxqlKGxC6ZWiakIWhqiMXNOatX"
 
   const handleClick = () => {
     setButtonText('Loading...');
 
     setTimeout(() => {
-        setButtonText('Submit');
+        setButtonText('Get Pictures');
     }, 2000); // Reverts back to 'Submit' after 2 seconds
   };
 
   const changeData = () => {
-    setHeroData('Loading...');
+    const response =  fetch("https://api.nasa.gov/planetary/earth/imagery?lon=-90.33&lat=29.78&date=2018-01-01&dim=0.15&api_key=sFbPQNVBu4cflCZxqlKGxC6ZWiakIWhqiMXNOatX");
+    const movies = response.json();
+    setHeroData(movies);
   };
 
   return (
     <main>
       <div>
-        <p id="title">My Website</p>
+        <h1 id="title">My Website</h1>
       </div>
       <button onClick={() => {
                   handleClick();
                   changeData();
                 }} class="button-35">{buttonText}</button>
-      <p>{heroData}</p>
+      <p>Example Images:</p>
+      <div>
+        <img class="example-photos" src="src\assets\ex1.png" alt="Satellite Picture" height={250} width={250}></img>
+        <img class="example-photos" src="src\assets\ex2.png" alt="Satellite Picture" height={250} width={250}></img>
+        <img class="example-photos" src="src\assets\ex3.png" alt="Satellite Picture" height={250} width={250}></img>
+        <img class="example-photos" src="src\assets\ex4.png" alt="Satellite Picture" height={250} width={250}></img>
+      </div>
+      <video autoPlay muted loop id="background-video">
+        <source src="src\assets\backgroundvid.mp4" type="video/mp4"></source>
+      </video>
     </main>
   )
 }
