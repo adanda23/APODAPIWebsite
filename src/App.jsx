330ac9af2f7a35b4dcbf237b3c225bbb9573e967
adanda23 <abhiramdanda@gmail.com>
2024-04-22 09:55:48 -0500

    Added input fields for longitude and latitude

--- src/App.jsx
+++ src/App.jsx
@@ -25,13 +25,23 @@ function App() {
       <>
         <h1 id="title">Satellite Picture Generator</h1>
       </>
-        <p>Example Images:</p>
+      <p>This webesite utilizes NASA's open Earth API. The API is powered by Google Earth Engine API and returns Landsat 8 imagery. You can configure the longitude and latitude values of the photo, as well as the date. Note that 
+        the returned picture may not match the supplied date exactly. Instead, the image closest to the supplied date is returned. </p>
+        <p class="center">Example Images:</p>
       <div class="wrapper">
         <img class="example-photos" src="src\assets\ex1.png" alt="Satellite Picture" height={250} width={250}></img>
         <img class="example-photos" src="src\assets\ex2.png" alt="Satellite Picture" height={250} width={250}></img>
         <img class="example-photos" src="src\assets\ex3.png" alt="Satellite Picture" height={250} width={250}></img>
         <img class="example-photos" src="src\assets\ex4.png" alt="Satellite Picture" height={250} width={250}></img>
       </div>
+        <form>
+          <label for="longitude">Longitude:  </label>
+          <input type="text" id="longitude" name="longitude"></input>
+        </form>
+        <form> 
+          <label for="Latitude">Latitude:  </label>
+          <input type="text" id="Latitude" name="Latitude"></input>
+        </form>
       <button onClick={() => {
                   handleClick();
                   changeData();
